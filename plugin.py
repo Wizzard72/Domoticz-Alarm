5ccab567ff5e0d344f13182388d5dd1bc7113e80
Wizzard72 <paulslats@gmail.com>
2020-06-07 22:39:34 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -169,7 +169,7 @@ class BasePlugin:
             self.Matrix[i][1] = node["Name"]
             i = i + 1
         
-        for count in range(str(Parameters["Mode1"])):
+        for count in range(int(Parameters["Mode1"])):
             Domoticz.Log(strName+"Alarm = "+self.Matrix[count][0]+" | "+str(self.Matrix[count][1])+" | "+str(self.Matrix[count][2])+" | "+self.Matrix[count][3]+" | "+self.Matrix[count][4])
         
         #for i in nodes:
