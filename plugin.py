0ece006a19216b99a24224cd2d926540ae837972
Wizzard72 <paulslats@gmail.com>
2020-06-07 22:14:12 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -187,28 +187,29 @@ class BasePlugin:
 
         
 def DomoticzAPI(APICall):
+    strName = "DomoticzAPI - "
     resultJson = None
     url = "http://{}:{}/json.htm?{}".format(Parameters["Address"], Parameters["Port"], parse.quote(APICall, safe="&="))
-    Domoticz.Log("Calling domoticz API: {}".format(url))
+    Domoticz.Log(strName+"Calling domoticz API: {}".format(url))
     try:
         req = request.Request(url)
         if Parameters["Username"] != "":
-            Domoticz.Log("Add authentification for user {}".format(Parameters["Username"]))
+            Domoticz.Log(strName+"Add authentification for user {}".format(Parameters["Username"]))
             credentials = ('%s:%s' % (Parameters["Username"], Parameters["Password"]))
             encoded_credentials = base64.b64encode(credentials.encode('ascii'))
             req.add_header('Authorization', 'Basic %s' % encoded_credentials.decode("ascii"))
 
         response = request.urlopen(req)
-        Domoticz.Log("Response status = "+str(response.status))
+        Domoticz.Log(strName+"Response status = "+str(response.status))
         if response.status == 200:
             resultJson = json.loads(response.read().decode('utf-8'))
             if resultJson["status"] != "OK":
-                Domoticz.Error("1Domoticz API returned an error: status = {}".format(resultJson["status"]))
+                Domoticz.Error(strName+"Domoticz API returned an error: status = {}".format(resultJson["status"]))
                 resultJson = None
         else:
-            Domoticz.Error("2Domoticz API: http error = {}".format(response.status))
+            Domoticz.Error(strName+"Domoticz API: http error = {}".format(response.status))
     except:
-        Domoticz.Error("3Error calling '{}'".format(url))
+        Domoticz.Error(strName+"Error calling '{}'".format(url))
     return resultJson
 
         
