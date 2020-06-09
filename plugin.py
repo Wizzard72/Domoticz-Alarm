0a37dd8b47a3b5d27d80debd9a86e2be2ebc1972
Wizzard72 <paulslats@gmail.com>
2020-06-09 21:58:25 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -433,8 +433,8 @@ class BasePlugin:
             
     def alarmModeChange(self, zoneNr, newStatus):
         strName = "alarmModeChange - "
-        Domoticz.Log(strName+"zoneNr = "+str(zoneNr)+" | newStatus = "+str(newStatus))
         zoneNrUnit = self.ALARM_ARMING_MODE_UNIT + int(zoneNr)
+        Domoticz.Log(strName+"zoneNr = "+str(zoneNr)+" | newStatus = "+str(newStatus)+ " | zoneNrUnit = "+zoneNrUnit)
         if newStatus == 0: # Normal
             # Reset Siren and Alarm Status
             #UpdateDevice(self.ALARM_MAIN_UNIT, 0, "Off")
