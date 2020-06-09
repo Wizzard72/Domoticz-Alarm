ea0ec8114fb36eb13c7876749172dcca7766ee97
Wizzard72 <paulslats@gmail.com>
2020-06-09 17:07:15 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -459,8 +459,8 @@ class BasePlugin:
         found_device = False
         for count in range(int(Parameters["Mode1"])):
             for item in Devices:
-                Domoticz.Log(strName+"Devices Name ="+Devices[item].Name[-19:])
-                if Devices[item].Name[-19:] == "Arming Mode (Z"+str(count)+")":
+                Domoticz.Log(strName+"Devices Name ="+Devices[item].Name[-20:])
+                if Devices[item].Name[-20:] == "Arming Mode (Z"+str(count)+")":
                         Domoticz.Log(strName+"Found device = "+"Arming Mode (Z"+str(count)+")")
                         found_device = True
             if found_device == False:
