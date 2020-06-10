de80e58adf3af2e8b19f71dd8816ed675cd3b746
Wizzard72 <paulslats@gmail.com>
2020-06-10 22:17:44 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -118,7 +118,7 @@ class BasePlugin:
         for zone in ZoneArmedAway:
             devicesIdx = zone.split(",")
             for devices in devicesIdx:
-                Domoticz.Log(strName+"device = "+device)
+                Domoticz.Log(strName+"10devices = "+devices)
                 if devices.lower() != "none" or devices != 0:
                     self.addToMatrix(TotalRows, zoneNr, "Armed Away", devices, "OFF", "NO", 0)
             zoneNr = zoneNr + 1
