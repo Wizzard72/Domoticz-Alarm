91e49550f1fed35b5ffd4cc615239fae8607c34c
Wizzard72 <paulslats@gmail.com>
2020-06-10 23:25:27 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -304,8 +304,8 @@ class BasePlugin:
             Domoticz.Log(strName+"deviceIDX"+str(deviceIDX))
             Domoticz.Log(strName+"self.getSwitchIDXStatus(deviceIDX) = "+self.getSwitchIDXStatus(deviceIDX))
             if self.getSwitchIDXStatus(deviceIDX) == "On":
-                Domoticz.Log(strName+"Gelukt")
                 if self.Matrix[row][4] not in "On,Normal":
+                    Domoticz.Log(strName+"Gelukt")
                     self.changeRowinMatrix(TotalRows, self.Matrix[row][3], "On", "Tripped", 10)
         for x in range(TotalRows):
             Domoticz.Log(strName+"Matrix: "+str(self.Matrix[x][0])+" | "+str(self.Matrix[x][1])+" | "+str(self.Matrix[x][2])+" | "+str(self.Matrix[x][3])+" | "+str(self.Matrix[x][4])+" | "+str(self.Matrix[x][5])+" | "+" | "+str(self.Matrix[x][5])+" | ")
