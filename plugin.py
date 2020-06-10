67dc31853b2649fed1bc42375ab92314de56c4f4
Wizzard72 <paulslats@gmail.com>
2020-06-10 21:36:07 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -481,7 +481,7 @@ class BasePlugin:
         
     def addToMatrix(self, TotalRows, ZoneNr, ArmMode, DeviceIdx, DeviceState, Changed, TimeChanged):
         strName = "addToMatrix - "
-        Domoticz.Log(strName+"TotalRows = "+TotalRows+ " ZoneNr = "+ZoneNr+" ArmMode = "+ArmMode+" DeviceIdx = "+DeviceIdx+" DeviceState = "+DeviceState+" Changed = "+Changed+" TimeChanged = "+TimeChanged)
+        Domoticz.Log(strName+"TotalRows = "+str(TotalRows)+ " ZoneNr = "+str(ZoneNr)+" ArmMode = "+ArmMode+" DeviceIdx = "+str(DeviceIdx)+" DeviceState = "+DeviceState+" Changed = "+Changed+" TimeChanged = "+TimeChanged)
         # Find free row number
         LastRow = 0
         for row in range(TotalRows):
