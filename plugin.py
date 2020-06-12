1cd65749dcd1dcf25d20f5429fbc2a6e1406a988
Wizzard72 <paulslats@gmail.com>
2020-06-12 22:17:21 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -133,17 +133,17 @@ class BasePlugin:
             Domoticz.Debug(strName+"Matrix: "+str(self.Matrix[x][0])+" | "+str(self.Matrix[x][1])+" | "+str(self.Matrix[x][2])+" | "+str(self.Matrix[x][3])+" | "+str(self.Matrix[x][4])+" | "+str(self.Matrix[x][5])+" | "+" | "+str(self.Matrix[x][5])+" | ")
         
         if Parameters["Mode1"] == 1:
-            ActivePIRSirenHome = 1
-            ActivePIRSirenAway = 1
+            self.ActivePIRSirenHome = 1
+            self.ActivePIRSirenAway = 1
         elif Parameters["Mode1"] == 2:
-            ActivePIRSirenHome = 1
-            ActivePIRSirenAway = 2
+            self.ActivePIRSirenHome = 1
+            self.ActivePIRSirenAway = 2
         elif Parameters["Mode1"] == 3:
-            ActivePIRSirenHome = 2
-            ActivePIRSirenAway = 1
+            self.ActivePIRSirenHome = 2
+            self.ActivePIRSirenAway = 1
         elif Parameters["Mode1"] == 4:
-            ActivePIRSirenHome = 2
-            ActivePIRSirenAway = 2
+            self.ActivePIRSirenHome = 2
+            self.ActivePIRSirenAway = 2
         
         
         Domoticz.Heartbeat(int(Parameters["Mode5"]))
