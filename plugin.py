3d9e80a3a493ad30083011156a825b5ed21b6a98
Wizzard72 <paulslats@gmail.com>
2020-06-12 22:50:16 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -306,15 +306,35 @@ class BasePlugin:
         #self.alarmEnable()
         #self.collectSensorData()
         
-        if self.sirenOn == True:
+        if self.Matrix[0][3] == "On":
             try:
-                timeDiff = datetime.now() - datetime.strptime(Devices[self.ALARM_MAIN_UNIT].LastUpdate,'%Y-%m-%d %H:%M:%S')
+                timeDiff = datetime.now() - datetime.strptime(Devices[255].LastUpdate,'%Y-%m-%d %H:%M:%S')
             except TypeError:
-                timeDiff = datetime.now() - datetime(*(time.strptime(Devices[self.ALARM_MAIN_UNIT].LastUpdate,'%Y-%m-%d %H:%M:%S')[0:6]))
+                timeDiff = datetime.now() - datetime(*(time.strptime(Devices[255].LastUpdate,'%Y-%m-%d %H:%M:%S')[0:6]))
             timeDiffSeconds = timeDiff.seconds
-            if timeDiffSeconds >= 45:
-                self.deactivateSiren()
+            Domoticz.Log(strName+"OverRide is on for: "+str(timeDiffSeconds)+" seconds")
+            if timeDiffSeconds >= self.override_time:
+                self.Matrix[0][3] = "Off"
+                self.Matrix[0][4] = "Yes"
         
+
+        try:
+            for zone in range(self.TotalZones):
+                zoneNr - self.ALARM_ARMING_STATUS_UNIT+zone
+                if Devices[ZoneNr].nValue == 40:
+                    timeDiff = datetime.now() - datetime.strptime(Devices[zoneNr].LastUpdate,'%Y-%m-%d %H:%M:%S')
+        except TypeError:
+            for zone in range(self.TotalZones):
+                zoneNr - self.ALARM_ARMING_STATUS_UNIT+zone
+                if Devices[ZoneNr].nValue == 40:
+                    timeDiff = datetime.now() - datetime(*(time.strptime(Devices[self.ALARM_MAIN_UNIT].LastUpdate,'%Y-%m-%d %H:%M:%S')[0:6]))
+        timeDiffSeconds = timeDiff.seconds
+        if timeDiffSeconds >= Devices[zoneNr].nValue:
+            self.activateSiren()
+        else:
+            self.deactivateSiren()
+                
+         
     def pollZoneDevices(self, TotalRows):
         strName = "pollZoneDevices - "
         APIjson = DomoticzAPI("type=scenes")
@@ -467,6 +487,9 @@ class BasePlugin:
                     Domoticz.Log(strName+"zoneNrUnit = "+str(zoneNrUnit))
                     UpdateDevice(zoneNrUnit, 0, "0") # Normal
         
+    def alarmAlert(self, entryDelay):
+        
+    
     def collectSensorData(self):
         strName = "collectSensorData - "
         jsonQuery = "type=scenes"
