d2d31a13b5095f80ff0a69261716ffd0ff9c82b4
Wizzard72 <paulslats@gmail.com>
2020-06-13 20:53:23 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -494,7 +494,7 @@ class BasePlugin:
     def setTrippedSensorTimer(self, TotalRows, DeviceIdx, TimeChanged):
         strName = "setTrippedSensorTimer - "
         for row in range(TotalRows):
-            if self.Matrix[row][3] == DeviceIdx and self.Matrix[row][4] == "Tripped" and self.Matrix[row][5] == "New":
+            if self.Matrix[row][3] == DeviceIdx and self.Matrix[row][4] == "On" and self.Matrix[row][5] == "New":
                 self.Matrix[row][5] = "Locked"
                 self.Matrix[row][6] = TimeChanged
                 Domoticz.Debug(strName+"Changed row "+str(row)+" to: DeviceState = "+DeviceState+" Changed = "+Changed+" Time Changed = "+str(TimeChanged))
