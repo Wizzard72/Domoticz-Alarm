502b653bc401d820d6eac7383e94d1dd4dc3e845
Wizzard72 <paulslats@gmail.com>
2020-06-18 19:01:49 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -563,7 +563,7 @@ class BasePlugin:
             except TypeError:
                 timeDiff = datetime.now() - datetime(*(time.strptime(Devices[ZoneID].LastUpdate,'%Y-%m-%d %H:%M:%S')[0:6]))
             timeDiffSeconds = timeDiff.seconds
-            if timeDiffSeconds >= Devices[exitDelay].nValue:
+            if timeDiffSeconds >= Devices[self.exitDelay].nValue:
                 if Devices[StatusID].nValue == 50: # Open sections
                     try:
                         timeDiff = datetime.now() - datetime.strptime(Devices[StatusID].LastUpdate,'%Y-%m-%d %H:%M:%S')
